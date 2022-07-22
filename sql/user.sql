d39ca472a51aeb62f0988b144575f7ae58e34047
Casper <solitaryreach@163.com>
2022-07-22 15:37:22 +0800

    feat: add user sql

---
+++ sql/user.sql
@@ -0,0 +1,9 @@
+create table user
+(
+    id       int(64) not null auto_increment,
+    username varchar(64)  not null,
+    password varchar(128) not null,
+    phone    varchar(18),
+    nickname varchar(20),
+    PRIMARY KEY (id)
+) engine=innodb default charset=utf8mb4;
