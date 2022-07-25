a52dc937598674bd539225ac8a244d9d7600ffe1
Casper <solitaryreach@163.com>
2022-07-25 10:31:04 +0800

    feat: remove useless code

--- internal/data/data.go
+++ internal/data/data.go
@@ -9,7 +9,7 @@ import (
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewGreeterRepo, NewUserRepo)
+var ProviderSet = wire.NewSet(NewData, NewUserRepo)
 
 // Data .
 type Data struct {
