74eb60cb413b611b5091f6e653cfd12ff8e863d0
Casper <solitaryreach@163.com>
2022-07-26 17:46:31 +0800

    feat: add comment

--- internal/biz/account.go
+++ internal/biz/account.go
@@ -13,11 +13,11 @@ var (
 )
 
 type User struct {
-	ID       int64
-	Username string
-	Password string
-	Nickname string
-	Avatar   string
+	ID       int64  // 用户ID
+	Username string // 用户名
+	Password string // 密码
+	Nickname string // 昵称
+	Avatar   string // 头像
 }
 
 //CheckPassword 校验密码是否正确
@@ -33,7 +33,9 @@ func (u *User) CheckPassword(ctx context.Context, pwd string, encryptService Enc
 }
 
 type UserRepo interface {
-	Fetch(ctx context.Context, id int64) (user *User, err error)
+	// FetchByUsername 获取指定用户名的用户的信息，如果用户不存在，则返回 ErrUserNotExist。
+	FetchByUsername(ctx context.Context, username string) (user *User, err error)
+	// Save 保存用户信息并返回用户的id。
 	Save(ctx context.Context, user *User) (id int64, err error)
 }
 
@@ -43,6 +45,7 @@ type AccountUseCase struct {
 	logger         *log.Helper
 }
 
+//NewAccountUseCase 创建一个AccountUseCase，依赖作为参数传入
 func NewAccountUseCase(logger log.Logger, userRepo UserRepo, encryptService EncryptService) *AccountUseCase {
 	return &AccountUseCase{
 		encryptService: encryptService,
@@ -56,6 +59,15 @@ func (a *AccountUseCase) Register(ctx context.Context, username, pwd string) (er
 	// 校验参数
 	if username == "" || pwd == "" {
 		return fmt.Errorf("注册失败：%w", ErrRegisterParamEmpty)
+	}
+	// 判断用户是否已经注册一次了
+	user, err := a.userRepo.FetchByUsername(ctx, username)
+	if err != nil && !errors.Is(err, ErrUserNotExist) {
+		log.Errorf("注册失败，参数[username: %s，pwd:%s]，err:%v", username, pwd, err)
+		return fmt.Errorf("注册失败")
+	}
+	if user != nil {
+		return fmt.Errorf("用户已经存在")
 	}
 	// 加密密码
 	encrypt, err := a.encryptService.Encrypt(ctx, []byte(pwd))
