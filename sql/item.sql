db5b5b2fc18229b969beeb8356fd5d1b130ac411
wangqunsheng <1227326164@qq.com>
2022-09-26 00:37:24 +0800

    增加seller_id

--- sql/item.sql
+++ sql/item.sql
@@ -5,6 +5,7 @@ create table item
   name varchar(256) not null default '',
   icon_url varchar(256) not null default '',
   price decimal(10, 2) not null default 0.0,
+  seller_id int(10) not null,
   id_del tinyint(1) not null default 0 comment '0.未删除 1.已删除',
   effect_begin timestamp not null default 0 comment '上架时间',
   effect_end timestamp not null default 0 comment '下架时间',
