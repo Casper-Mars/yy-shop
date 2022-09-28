ca267904d3e2ff081eacecbbee4db69ca5991f86
wangqunsheng <1227326164@qq.com>
2022-09-29 01:27:40 +0800

    pageToken

--- internal/service/item.go
+++ internal/service/item.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	v1 "yy-shop/api/v1"
 	"yy-shop/internal/biz"
 
@@ -14,6 +16,23 @@ type itemService struct {
 	productMgr *biz.ProductMgr
 }
 
+type pageToken struct {
+	TokenID  uint32 `json:"token_id"`  // 已获取的最大id
+	ItemName string `json:"item_name"` // 商品名称
+}
+
+func (p *pageToken) string() string {
+	if p == nil {
+		return ""
+	}
+	jsonByte, err := json.Marshal(p)
+	if err != nil {
+		log.Errorf("pageToken failed to Marshal, err:%v", err)
+		return ""
+	}
+	return base64.StdEncoding.EncodeToString(jsonByte)
+}
+
 func NewProductServer(logger log.Logger, productMgr *biz.ProductMgr) v1.ProductServer {
 	return &itemService{
 		log:        log.NewHelper(logger),
@@ -23,36 +42,61 @@ func NewProductServer(logger log.Logger, productMgr *biz.ProductMgr) v1.ProductS
 
 func (a *itemService) SearchItem(ctx context.Context, request *v1.SearchItemRequest) (*v1.SearchItemResponse, error) {
 	out := &v1.SearchItemResponse{}
-	pageToken, err := a.parseToken(ctx, request.GetPageToken())
-	if err != nil {
-		a.log.Errorf("SearchItem failed to parseToken, err:%v", err)
-		return out, err
-	}
-	itemInfoList, err := a.productMgr.SearchItem(ctx, request.GetName(), pageToken, request.GetPageSize())
-	if err != nil {
+
+	itemInfoList, err := a.productMgr.SearchItem(ctx, request.GetName(), a.parseToken(ctx, request.GetPageToken()),
+		request.GetPageSize())
+	if err != nil || len(itemInfoList) == 0 {
 		a.log.Errorf("SearchItem failed, request:%v, err:%v", request, err)
 		return out, err
 	}
-
-	newToken, err := a.genNewToken(ctx, request.GetPageToken(), request.GetPageSize())
-	if err != nil {
-		a.log.Errorf("SearchItem failed to genNewToken, err:%v", err)
-	}
-
 	return &v1.SearchItemResponse{
 		ItemList:  a.convertItemInfo2Pb(itemInfoList),
-		PageToken: newToken,
+		PageToken: a.genNewToken(ctx, request.GetName(), request.GetPageToken(), itemInfoList[len(itemInfoList)-1].ItemId),
 	}, nil
 }
 
-func (a *itemService) parseToken(ctx context.Context, token string) (uint32, error) {
-	return 0, nil
+func (a *itemService) parseToken(ctx context.Context, token string) uint32 {
+	if token == "" {
+		return 0
+	}
+
+	jsonByte, err := base64.StdEncoding.DecodeString(token)
+	if err != nil {
+		log.Errorf("DecodeString failed, err:%v", err)
+		return 0
+	}
+	pt := &pageToken{}
+	err = json.Unmarshal(jsonByte, pt)
+	if err != nil {
+		log.Errorf("Unmarshal failed, err:%v", err)
+		return 0
+	}
+	return pt.TokenID
 }
 
-func (a *itemService) genNewToken(ctx context.Context, token string, pageSize uint32) (string, error) {
-	return "", nil
+func (a *itemService) genNewToken(ctx context.Context, itemName, token string, tokenID uint32) string {
+	if token == "" {
+		return a.initToken(ctx, itemName, tokenID)
+	}
+	jsonByte, err := base64.StdEncoding.DecodeString(token)
+	if err != nil {
+		log.Errorf("DecodeString failed, err:%v", err)
+		return ""
+	}
+	pt := &pageToken{}
+	err = json.Unmarshal(jsonByte, pt)
+	if err != nil {
+		log.Errorf("Unmarshal failed, err:%v", err)
+		return ""
+	}
+	pt.TokenID = tokenID
+	return pt.string()
 }
 
+func (a *itemService) initToken(ctx context.Context, itemName string, token uint32) string {
+	pt := &pageToken{TokenID: token, ItemName: itemName}
+	return pt.string()
+}
 func (a *itemService) convertItemInfo2Pb(itemInfoList []*biz.ItemInfoWithSeller) []*v1.Item {
 	return []*v1.Item{}
 }
